e170476a9a60c105bbeb23ee028d45649e2c0b9a
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-06-04 04:09:24 +0000

    Add code to style 9 boxes to change color to red on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,9 @@
 //     $(this).removeClass('makeBlue').addClass('makeRed');
 // });
 
+$(document).ready(function() {
+    $('.box').on('click', function() {
+       var classNames =  $(this).attr('class').split(" ");
+       $("." + classNames[1]).css('background-color', 'red');
+    });
+});
